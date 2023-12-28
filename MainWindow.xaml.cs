7528d854e09068c0406453509a5a5221cd04f51c
Christopher M Green <cgvoice35@gmail.com>
2023-12-28 16:47:07 -0600

    Add format selection

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 using Microsoft.UI.Xaml;
-using Microsoft.UI.Xaml.Controls;
-using Microsoft.UI.Xaml.Controls.Primitives;
-using Microsoft.UI.Xaml.Data;
-using Microsoft.UI.Xaml.Input;
-using Microsoft.UI.Xaml.Media;
-using Microsoft.UI.Xaml.Navigation;
+using SolresolConverter;
+using SolresolConverter.Converter;
 using System;
-using System.Collections.Generic;
-using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,11 +17,25 @@ namespace SolresolConverterApp
         public MainWindow()
         {
             this.InitializeComponent();
+            var _enumVal = Enum.GetValues(typeof(SolresolFormat)).Cast<SolresolFormat>();
+            sorsoInFormat.ItemsSource = _enumVal.ToList();
+            sorsoOutFormat.ItemsSource = _enumVal.ToList();
+            sorsoInFormat.SelectedItem = SolresolFormat.Sorso;
+            sorsoOutFormat.SelectedItem = SolresolFormat.Ses;
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void convertButton_Click(object sender, RoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            var inFormat = sorsoInFormat.SelectedItem as SolresolFormat?;
+            var outFormat = sorsoOutFormat.SelectedItem as SolresolFormat?;
+            string output = Converter.ConvertSolresol(sorsoIn.Text, inFormat.Value, outFormat.Value);
+            sorsoOut.Text = output;
+        }
+
+        private void clearButton_Click(object sender, RoutedEventArgs e)
+        {
+            sorsoIn.Text = string.Empty;
+            sorsoOut.Text = string.Empty;
         }
     }
 }
