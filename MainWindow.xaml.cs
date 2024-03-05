93c4bf07bf3b65a8f06ebf93cab1b56ae6e8b46a
Christopher M Green <cgvoice35@gmail.com>
2024-03-04 23:23:08 -0600

    Add optional compound word entry

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,13 @@
 using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
 using SolresolConverter;
 using SolresolConverter.Converter;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Windows.UI.Popups;
+using static SolresolConverter.Converter.Converter;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -21,21 +26,43 @@ namespace SolresolConverterApp
             sorsoInFormat.ItemsSource = _enumVal.ToList();
             sorsoOutFormat.ItemsSource = _enumVal.ToList();
             sorsoInFormat.SelectedItem = SolresolFormat.Sorso;
-            sorsoOutFormat.SelectedItem = SolresolFormat.SesCmgreen;
+            sorsoOutFormat.SelectedItem = SolresolFormat.Ses;
+            // TODO Re-enable when more formats are available.
+            sorsoInFormat.IsEnabled = false;
+            compoundWordsBox.Visibility = (sorsoOutFormat.SelectedItem as SolresolFormat?)
+                .Value == SolresolFormat.SesCmgreen ? Visibility.Visible : Visibility.Collapsed;
         }
 
         private void convertButton_Click(object sender, RoutedEventArgs e)
         {
             var inFormat = sorsoInFormat.SelectedItem as SolresolFormat?;
             var outFormat = sorsoOutFormat.SelectedItem as SolresolFormat?;
-            string output = Converter.ConvertSolresol(sorsoIn.Text, inFormat.Value, outFormat.Value);
-            sorsoOut.Text = output;
+            List<CompoundWord> temp = new();
+            string[] compoundWordContents = compoundWordsBox.Text.Split(';');
+            foreach ( var compoundWord in compoundWordContents ) {
+                temp.Add(new CompoundWord(compoundWord));
+            }
+            try
+            {
+                string output = ConvertSolresol(sorsoIn.Text, inFormat.Value, outFormat.Value, temp);
+                sorsoOut.Text = output;
+            } catch (NotImplementedException)
+            {
+                sorsoOut.Text = "Sorry, this format is not available yet!";
+            }
         }
 
         private void clearButton_Click(object sender, RoutedEventArgs e)
         {
             sorsoIn.Text = string.Empty;
             sorsoOut.Text = string.Empty;
+            compoundWordsBox.Text = string.Empty;
+        }
+
+        private void sorsoOutFormat_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            compoundWordsBox.Visibility = (sorsoOutFormat.SelectedItem as SolresolFormat?)
+                .Value == SolresolFormat.SesCmgreen ? Visibility.Visible : Visibility.Collapsed;
         }
     }
 }
